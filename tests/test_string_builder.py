6ac154c4d266049e75f41b9cb0229f842d842572
samneat <samjneat@gmail.com>
2024-01-30 11:53:20 +0000

    create StringBuilder class and tests

---
+++ tests/test_string_builder.py
@@ -0,0 +1,29 @@
+from lib.string_builder import *
+
+def test_string_builder_outputs_string():
+    string = StringBuilder()
+    string.add("Sam can code")
+    result = string.output()
+    assert result == "Sam can code"
+
+def test_string_builder_outputs_string_length():
+    string = StringBuilder()
+    string.add("hello")
+    result = string.size()
+    assert result == 5
+
+def test_adding_mulitple_strings_returns_concatenated_strings():
+    string = StringBuilder()
+    string.add("hello")
+    string.add(' ')
+    string.add('world')
+    result = string.output()
+    assert result == "hello world"
+
+def test_return_length_of_concatenated_strings():
+    string = StringBuilder()
+    string.add("hello")
+    string.add(' ')
+    string.add('world')
+    result = string.size()
+    assert result == 11
